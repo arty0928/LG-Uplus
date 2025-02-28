3549a11139cac5b7e3762918002026fa86f6dbf3
arty0928 <arty0928@konkuk.ac.kr>
2025-02-28 17:54:28 +0900

    [Algo] 14405 피카츄

--- 00_Algo_Practice/src/baekjoon/Main_14405_S5_피카츄.java
+++ 00_Algo_Practice/src/baekjoon/Main_14405_S5_피카츄.java
@@ -4,7 +4,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 /*
- * 그냥 if문으로 pi contains -> replace,,, 다 했는데도 문자열에 남아 있으면 NO
+ * 그냥 if문으로 replace,,, 다 했는데도 문자열에 남아 있으면 NO
+ * 
  */
 public class Main_14405_S5_피카츄 {
 
@@ -12,15 +13,64 @@ public class Main_14405_S5_피카츄 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String line = br.readLine();
 		
-			// 이렇게 나눠서 한다며 replace 는 정답, replaceAll은 오답
-			line = line.replace("pi", "");
-			line = line.replace("ka", "");
-			line = line.replace("chu", "");
+			// 오답 반례 kpia
+		/*
+		 * 
+//		 */
+//			line = line.replace("pi", "");
+//			System.out.println("1번 line = " + line);
+//			
+//			line = line.replace("ka", "");
+//			System.out.println("2번 line = " + line);
+//			
+//			line = line.replace("chu", "");
+//			System.out.println("3번 line = " + line);
 			
 			
-			// 이것만 하면 정답
+			// 오답 반례 kpia
+		/*
+		 * replaceAll 은 [정규식 기반]으로 동작 
+		 * 		: 의도한 대로 순차적으로 문자열을 처리하지 않고 일치하는 패턴을 찾아서 모두 제거
+		 * 
+		 * 		여기서 pi, ka, chu 를 정규식 패턴으로 사용 
+		 * 		-> 정규식 패턴으로 사용하지 않기 위해 replace 사용
+		 * 
+		 * 반례 : kpia -> YES 나옴 (정답: NO)
+		 */
+			line = line.replaceAll("pi", "");
+			System.out.println("4번 line = " + line);
+			line = line.replaceAll("ka", "");
+			System.out.println("4번 line = " + line);
+			line = line.replaceAll("chu", "");
+			System.out.println("4번 line = " + line);
+		
+			
+			// 정답
 //			line = line.replaceAll("pi|ka|chu", ""); // pi, ka, chu를 동시에 치환
 		
+			
+			/*
+			 * 성공
+			 */
+			line = line.replaceAll("pi", " ");
+			System.out.println("4번 line = " + line);
+			line = line.replaceAll("ka", " ");
+			System.out.println("4번 line = " + line);
+			line = line.replaceAll("chu", " ");
+			System.out.println("4번 line = " + line);
+		
+			/*
+			 * 성공
+			 */
+			line = line.replace("pi", "");
+			System.out.println("4번 line = " + line);
+			line = line.replace("ka", "");
+			System.out.println("4번 line = " + line);
+			line = line.replace("chu", "");
+			System.out.println("4번 line = " + line);
+		
+			
+			
 		if(line.equals("")) System.out.println("YES");
 		else System.out.println("NO");
 	}
