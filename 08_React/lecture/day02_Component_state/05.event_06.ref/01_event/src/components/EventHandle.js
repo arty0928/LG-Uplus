e214fe695d832edde7a17c86c8bbc8a801175ccb
arty0928 <arty0928@konkuk.ac.kr>
2025-04-02 15:52:24 +0900

    [08_React] 4/2 [구현퀴즈]  (함수형 컴포넌트) username, message를 한번에 입력받고 처리하도록 객체로 만들어보기

--- 08_React/lecture/day02_Component_state/05.event_06.ref/01_event/src/components/EventHandle.js
+++ 08_React/lecture/day02_Component_state/05.event_06.ref/01_event/src/components/EventHandle.js
@@ -1,66 +1,62 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-export default class EventHandle extends Component {
-  //바벨이 생성자 내부에 만드는 코드로 변환한다.
-  //est : empty state 
-  state = {
-    message: "",
-    username: "",
-  };
+/**
+ * [구현 퀴즈] : username, message를 한번에 입력받고 처리하도록 객체로 만들어보기 
+ */
+const EventHandle = () => {
 
-  handleChange = (e) => {
-    this.setState({
-      // JSON 객체 내부에서 key를 동적으로 변경할 때는 [key]
-      [e.target.name]: e.target.value,
-    });
+  const [data, setData] = useState({ username: '', message: '' });
+  
+  const { username, message } = data;
+
+  const handleChange = (e) => {
+    console.log(e);
+    // 변경하려는 state만 남고 다른 state는 모두 제거됨
+    // setData({ [e.target.name]: e.target.value }); // 바로 바꾸면 안됨 (불변성)
+
+    /* 
+      불변성 유지하기 위해 새로운 객체 생성하고 해당 속성만 변경해야 한다.
+        1. 기존 객체 복제
+        2. 특정 state만 변경
+    */
+    const newForm = {
+      ...data,  //기본 state를 복제
+      [e.target.name]: e.target.value
+    } //이벤트가 발생한 state만 변경하기
+    setData(newForm);
   };
   
-  handleClick = () => {
-    this.setState({
-      message: "",
-      username: "",
-    });
+  
+  const hanldeClick = () => {
+    setData({ username: "", message: "" });
   };
   
-  handleKeyDown = (e) => {
-    console.log(e.key);
-    if (e.key === "Enter") {  //key가 "Enter"인 경우만
-      console.log('hanlekeydown......');
-      this.handleClick();
-    }
-  };
+  return (
+    <div>
+      <input
+        type="text"
+        value={username}
+        onChange={handleChange}
+        name="username"
+        placeholder="user name"></input>
+      
+      <input
+        type="text"
+        value={message}
+        onChange={handleChange}
+        name="message"
+        placeholder="message"></input>
+      
+      <button type="button" onClick={hanldeClick}>
+        확인
+      </button>
 
-  
-
-  render() {
-    return (
       <div>
-        <input
-          type="text"
-          value={this.state.username}
-          onKeyDown={this.handleKeyDown}
-          onChange={this.handleChange}
-          name="username"
-          placeholder="user name"></input>
-        
-        <input
-          type="text"
-          value={this.state.message}
-          onKeyDown={this.handleKeyDown}
-          onChange={this.handleChange}
-          name="message"
-          placeholder="message"></input>
-        
-        <button type="button" onClick={this.handleClick}>
-          확인
-        </button>
-        
-        <div>
-          {this.state.username} {this.state.username !== "" && ":"}
-          {this.state.message}
-        </div>
-        
+        {username} {username !== "" && ":"}
+        {message}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default EventHandle;
