101ce81faacc321b539a0738f7022cdd922c74e7
arty0928 <arty0928@konkuk.ac.kr>
2025-04-03 11:18:48 +0900

    tmp

--- 08_React/lecture/day02_Component_state/04.state/01_statetest/src/App.js
+++ 08_React/lecture/day02_Component_state/04.state/01_statetest/src/App.js
@@ -4,6 +4,8 @@ import Say from './Say';
 
 const App = () => {
     const [count, setCount] = useState(0);
+    const increase = () => setCount(count + 1);
+    const decrease = () => setCount(count + 1);
     
     return (
         <div>
