e25fc7882df901e4eaa97be545fda946dcb8b073
arty0928 <arty0928@konkuk.ac.kr>
2025-04-03 15:47:49 +0900

    [08_React] 4/3 MultiInput 실습

--- 08_React/lecture/day03_event_lifecycle/05.event_06_ref/04_form/src/component/MultiInput.js
+++ 08_React/lecture/day03_event_lifecycle/05.event_06_ref/04_form/src/component/MultiInput.js
@@ -1,17 +1,40 @@
 import React, { Component } from "react";
 
 class MultiInput extends Component {
+  state = {
+    isGoing: true,      //check box는 true/false로 처리해야 한다.
+    numberOfGuests:2,   //number input 양식은 숫자로 초기화
+  };
+
+  handleChange = (e) => {
+    const { type, id } = e.target;
+    
+    // 이벤트가 발생한 대상이 checkbox인지 number인지에 따라 value값을 처리
+    const value = type === 'checkbox' ? e.target.checked : e.target.value;
+    this.setState({
+      [id] : value,
+    })
+  };
+  
   render() {
     return (
       <form>
         <label for="isGoing">
           Is going:
-          <input id="isGoing" type="checkbox" />
+          <input id="isGoing"
+            type="checkbox"
+            checked={this.state.isGoing}
+            onChange={this.handleChange} />
         </label>
         <br />
         <label for="numberOfGuests">
           Number of guests:
-          <input id="numberOfGuests" type="number" />
+          <input
+            id="numberOfGuests"
+            type="number"
+            value={this.state.numberOfGuests}
+            onChange={this.handleChange}  
+          />
         </label>
       </form>
     );
