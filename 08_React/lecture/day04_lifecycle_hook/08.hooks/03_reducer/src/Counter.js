36fb1cd5b603f279af3d9abae7a81ba04a70bba2
arty0928 <arty0928@konkuk.ac.kr>
2025-04-04 16:11:22 +0900

    [08_React] useReducer 실습

--- 08_React/lecture/day04_lifecycle_hook/08.hooks/03_reducer/src/Counter.js
+++ 08_React/lecture/day04_lifecycle_hook/08.hooks/03_reducer/src/Counter.js
@@ -8,5 +8,60 @@ reducer   : dispatch에 의해 호출되어 state를 관리할 함수
 state객체  : 관리할 state
 
 reducer(state, action)
-
 */
+
+import React, { useReducer, useState } from 'react';
+
+const INCREMENT = 1;
+const DECREMENT = 2;
+const INIT = 3;
+
+
+const reducer = (state, action) => {
+    console.log("reducer....action : ", action);
+    switch (action.type) {
+        case INCREMENT:
+            return { value: state.value + parseInt(action.value) }
+        case DECREMENT:
+            return { value: state.value - parseInt(action.value) }
+        case INIT:
+            return { value: 1 };
+        default:
+            return state; // 지정한 action이 아닌 다른 action이 요청된 경우 state는 변하지 않는 state를 전달
+    }
+};
+
+const Counter = () => {
+    const [state, dispatch] = useReducer(reducer, { value: 0 })
+    const [step, setStep] = useState(1);
+
+    return (
+        <div>
+            <p>현재 카운터 값은 {state.value} 입니다. </p>
+            step: {"  "}
+            <input
+                type='text'
+                value={step}
+                onChange={(e) => {
+                    setStep(e.target.value)
+                }}
+            />
+            <button onClick={ () => {
+                dispatch({ type: INCREMENT, value: step });
+            }}
+            >+</button>
+
+            <button onClick={ () => {
+                dispatch({ type: DECREMENT, value: step });
+            }}
+            >-</button>
+
+            <button onClick={ () => {
+                dispatch({ type: INIT, value: step });
+            }}
+            >초기화</button>
+        </div>
+    );
+};
+
+export default Counter;
