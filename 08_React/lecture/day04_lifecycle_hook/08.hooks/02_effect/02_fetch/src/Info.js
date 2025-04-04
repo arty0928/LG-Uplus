67bac9e2f3d9b2fea855b5bb9e11e1598cc004a7
arty0928 <arty0928@konkuk.ac.kr>
2025-04-04 15:23:00 +0900

    [08_React] 4/4 lifecycle_hook 실습

--- 08_React/lecture/day04_lifecycle_hook/08.hooks/02_effect/02_fetch/src/Info.js
+++ 08_React/lecture/day04_lifecycle_hook/08.hooks/02_effect/02_fetch/src/Info.js
@@ -14,3 +14,51 @@
       </ul>
     </div>
 */
+
+
+import React, { useEffect, useState } from 'react';
+import './Info.css';
+
+const Info = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoadong] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("사용자 정보를 조회 중에 오류가 발생했습니다.");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setUsers(data);
+      })
+      .catch((err) => {
+        setError(err.message);
+      })
+      .finally(() => {
+        setLoadong(false);
+      });
+  }, []);
+
+  if (loading) return <p className='loading'>Loading....</p>;
+  if (error) return <p className='error'>오류 발생 : {error}</p>;
+  return (
+    <div className="container">
+      <h1 className="title">User List</h1>
+      <ul className="user-list">
+        {users.map((user) => (
+          <li key="" className="user-item">
+            <span className="user-name"></span>
+            <br />
+            <span className="user-email"></span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Info;
