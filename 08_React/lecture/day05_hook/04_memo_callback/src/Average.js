9e29ad966d2a4476de30f4c7d2140ddefcc9edfb
arty0928 <arty0928@konkuk.ac.kr>
2025-04-07 09:27:08 +0900

    [08_React] 4/7 Hook 수업 자료

---
+++ 08_React/lecture/day05_hook/04_memo_callback/src/Average.js
@@ -0,0 +1,8 @@
+/*
+  array.reduce(callback, initialValue)
+    callback(accumulator, current, currentIndex, array)
+      accumulator : reduce를 한 이전 결과, 처음인 경우  initialValue가 있으면 initialValue 
+      current : 처리할 현재 요소
+      currentIndex : 처리할 현재 요소의 index,  initialValue가 있으면 0, 없으면 1부터 시작
+      array : reduce를 시작한 배열 
+*/
