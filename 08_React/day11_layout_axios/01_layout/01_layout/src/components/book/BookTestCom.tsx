2bd94027fdcb89ce696545dd1aa019c5ec139f66
arty0928 <arty0928@konkuk.ac.kr>
2025-04-15 10:19:43 +0900

    [08_React] 01_layout

---
+++ 08_React/day11_layout_axios/01_layout/01_layout/src/components/book/BookTestCom.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import React from 'react';
+
+
+const BookTestCom = () => {
+    return (
+        <div>
+            <h1>BookTestCom!!</h1>
+        </div>
+    );
+};
+
+export default BookTestCom;
