edf9b394a88b346e1724dc70e469c6fa94a91c41
arty0928 <arty0928@konkuk.ac.kr>
2025-04-17 14:54:15 +0900

    [08_React] axios - server 07_react_query

--- 08_React/day13_axios_server/07_react_query/src/app/query/regist/page.tsx
+++ 08_React/day13_axios_server/07_react_query/src/app/query/regist/page.tsx
@@ -1,13 +1,35 @@
 "use client";
+import { useMutation } from "@tanstack/react-query";
 import styles from "./UseMutationEx.module.scss";
+import { handleApi } from "@/utils/handleApi";
+import { createUser } from "@/service/query";
+import { useRef } from "react";
 const UseMutaionEx = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+
+  const mutation = useMutation({
+    mutationFn: createUser,
+    onSuccess: (data) => {
+      alert(`등록 선공 id: ${data.id}`);
+    },
+  })
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const name = nameRef.current?.value || "";
+    const email = emailRef.current?.value || "";
+
+    mutation.mutate({ name, email });
+  }
   return (
     <form onSubmit={handleSubmit} className={styles.form}>
-      <input type="text" placeholder="Name" className={styles.input} required />
+      <input type="text" ref={nameRef} placeholder="Name" className={styles.input} required />
       <br />
-      <input type="email" placeholder="Email" className={styles.input} required />
+      <input type="email" ref = {emailRef} placeholder="Email" className={styles.input} required />
       <br />
-    </form>
+      <button type ="submit" className={styles.submitButton}></button>
+     </form>
   );
 };
 export default UseMutaionEx;
