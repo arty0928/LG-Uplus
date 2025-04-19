e2640d3ee1b93bbc4a35f86a9a94f171601c3014
arty0928 <arty0928@konkuk.ac.kr>
2025-04-19 23:53:42 +0900

    [Algo] coin change _ leetcode

---
+++ 00_Algo_Practice/src/dp/coin_change_leetcode.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        int[] dp = new int[amount+1];
+        Arrays.fill(dp, Integer.MAX_VALUE - 1);
+        dp[0]=0;
+
+        for(int coin : coins){
+            for(int i = coin; i <= amount; i++){
+                dp[i] = Math.min(dp[i], dp[i-coin] + 1); 
+            }
+        }
+        return dp[amount] == Integer.MAX_VALUE - 1 ? -1 : dp[amount];
+    }
+}
